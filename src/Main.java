f49b672621f014883e4aa006e22a1e825a268b46
Thijsku <thijs.kuilman@gmail.com>
2015-09-24 01:12:05 +0200

    Start van MysSQL connection
    Vanaf nu wordt dit verder uitgewerkt in mijn eigen branch

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,30 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 public class Main {
 
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+    public static void main(String[] args){
+        String url = "jdbc:mysql://localhost:3306/";
+        String dbName = "unwdmi";
+        String driver = "com.mysql.jdbc.Driver";
+        String userName = "root";
+        String password = "";
+        try {
+            Class.forName(driver).newInstance();
+            Connection conn = DriverManager.getConnection(url + dbName, userName, password);
+
+            conn.createStatement("SELECT name FROM stations")
+
+            conn.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        try {
+            conn.
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
