6fde6f2eee4a7baefac18f0e51f1744f146eee31
Thijsku <thijs.kuilman@gmail.com>
2015-09-24 02:55:12 +0200

    DatabaseConnect class + MySQL library
    Eerste implementatie van DatabaseConnect is gemaakt. Je kunt verbinden
    met een database en je kunt vervolgens SQL statements uitvoeren m.b.v
    methoden. Om een connectie tot stand te kunnen brengen, gebruiken we
    'mysql-connector-java-5.0.8'. Deze library heb ik ook in deze Git gezet,
    zodat jullie het gemakkelijk in je IDE kunnen toevoegen.

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,19 @@
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class Main {
 
+    public static DatabaseConnect database;
+    private static ResultSet sqloutput;
 
-    public static void main(String[] args){
+    public static void main(String[] args) throws SQLException {
+        database = new DatabaseConnect();
+
+//        Test statement
+//        sqloutput = database.excecuteSQL("SELECT name FROM stations WHERE country = 'AFGHANISTAN';");
+//        while (sqloutput.next()) {
+//            System.out.println(sqloutput.getString("name"));
+//        }
 
     }
 }
