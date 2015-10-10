f21c4801c74b8b1cb046a78b999348395bb7ba4d
SuperDicq <superdicq@Kyouko>
2015-10-10 17:57:44 +0200

    Clean up and closed serverSocket with finally{} block

--- src/Server.java
+++ src/Server.java
@@ -4,7 +4,6 @@
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.sql.Connection;
 
 public class Server {
     public static DatabaseConnect database;
@@ -12,27 +11,28 @@ public class Server {
      * Make a new server socket and wait for clients to connect. Once a client connects with the server, a new instance of ServerHandler will be created.
      */
 
-    public static void main(String args[]) {
+    public static void main(String args[]) throws IOException{
         // Listen to server port (7789 is default port of Unwdmi generator)
         int portNumber = 7789;
         database = new DatabaseConnect();
+        ServerSocket serverSocket = new ServerSocket(portNumber);
 //        XMLParser xmparser = new XMLParser();
 
         // Await connections of clients
         try {
-            ServerSocket serverSocket = new ServerSocket(portNumber);
+;
             Socket socket;
-
             System.out.println("Server started on port " + portNumber);
 
             while (true) {
                 socket = serverSocket.accept();
                 new Thread(new ServerHandler(socket)).start();
+                //serverSocket.close();
             }
-
-        // Catch potential exceptions
-        } catch(IOException io){
-            io.printStackTrace();
+        
+        }finally{
+        	serverSocket.close();
+            
         }
     }
 
